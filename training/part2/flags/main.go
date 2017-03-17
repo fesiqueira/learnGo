f187ebdb9395b440e20e0ae230c9b6885df4fd69
Felipe Siqueira Pinheiro <fel.siqueirapinheiro@gmail.com>
2017-03-17 18:15:28 -0300

    Teste

--- training/part2/flags/main.go
+++ training/part2/flags/main.go
@@ -7,6 +7,7 @@ import (
 
 // note, that variables are pointers
 var intFlag = flag.Int("port", 0, "Flag usage example: port")
+var listFlag = flag.String("l", showServers(), "Used to show all available servers")
 
 func main() {
 	flag.Parse()
