79aa067ce7b9aa1ecc8e8aac6f53a0a53b4af252
Felipe Siqueira Pinheiro <fel.siqueirapinheiro@gmail.com>
2017-03-17 20:55:55 -0300

    Initialized a flag usage example

--- training/part2/flags/main.go
+++ training/part2/flags/main.go
@@ -3,7 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 )
+
+func checkError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func showServers() string {
+	serverFile := "./servers.txt"
+
+	fileContent, err := ioutil.ReadFile(serverFile)
+	checkError(err)
+
+	return string(fileContent)
+}
 
 // note, that variables are pointers
 var intFlag = flag.Int("port", 0, "Flag usage example: port")
@@ -11,5 +28,7 @@ var listFlag = flag.String("l", showServers(), "Used to show all available serve
 
 func main() {
 	flag.Parse()
+	fmt.Println(*listFlag)
 	fmt.Println("Server running on port:", *intFlag)
+	fmt.Println(flag.Args())
 }
