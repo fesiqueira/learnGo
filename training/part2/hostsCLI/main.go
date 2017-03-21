d5a7dcf3c266a9cd7506faa57b7c27fc4854c7cd
Felipe Siqueira Pinheiro <fel.siqueirapinheiro@gmail.com>
2017-03-21 18:27:11 -0300

    optionAndHost done

--- training/part2/hostsCLI/main.go
+++ training/part2/hostsCLI/main.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
 func optionAndHost() (string, string) {
 	var args [2]string
-
-	// Initializing a variable that holds only the arguments (except the program location)
-	osArgs := os.Args[1:]
-
-	// Initializing a variable that holds the total of arguments passed
-	numArgs := len(osArgs)
-
-	// Limiting to 2 arguments
-	switch {
+	osArgs := os.Args[1:]  // Initializing a variable that holds only the arguments (except the program location)
+	numArgs := len(osArgs) // Initializing a variable that holds the total of arguments passed
+	switch {               // Limiting to 2 arguments
 	case numArgs == 0:
 		fmt.Println("Too few arguments, use -h")
-	case len(os.Args[1:]) > 2:
+	case numArgs > 2:
 		fmt.Println("Too much arguments, use -h")
 	default:
 		for i := 0; i < numArgs; i++ {
@@ -28,6 +24,20 @@ func optionAndHost() (string, string) {
 	return args[0], args[1]
 }
 
+func printHelp() {
+	fmt.Println("Help log")
+}
+
 func main() {
-	fmt.Println(optionAndHost())
+	option, _ := optionAndHost()
+	switch option {
+	case "-l":
+		fileBytes, err := ioutil.ReadFile("./servers.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(fileBytes))
+	case "-h":
+		printHelp()
+	}
 }
