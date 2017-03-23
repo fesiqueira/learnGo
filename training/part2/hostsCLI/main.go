b263f4542c7e0c5b28356e0d8177d7a675e02e97
Felipe Siqueira Pinheiro <fel.siqueirapinheiro@gmail.com>
2017-03-23 18:25:04 -0300

    userArgs return an slice with the user's arguments

--- training/part2/hostsCLI/main.go
+++ training/part2/hostsCLI/main.go
@@ -2,40 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 )
 
-func userArgs() (option string, hostname string) {
-	var args [2]string
+func userArgs(nArgs int) (arguments []string) {
+	var args = make([]string, nArgs)
 	osArgs := os.Args[1:]  // Initializing a variable that holds only the arguments (except the program location)
 	numArgs := len(osArgs) // Initializing a variable that holds the total of arguments passed
 	switch {               // Limiting to 2 arguments
 	case numArgs == 0:
 		fmt.Println("Too few arguments, use -h")
-	case numArgs > 2:
+	case numArgs > nArgs:
 		fmt.Println("Too much arguments, use -h")
 	default:
 		copy(args[:], osArgs)
 	}
-	return args[0], args[1]
-}
-
-func printHelp() {
-	fmt.Println("Help log")
+	return args
 }
 
 func main() {
-	option, _ := userArgs()
-	switch option {
-	case "-l":
-		fileBytes, err := ioutil.ReadFile("./servers.txt")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(fileBytes))
-	case "-h":
-		printHelp()
-	}
+	arguments := userArgs(2)
+	fmt.Println(arguments)
 }
