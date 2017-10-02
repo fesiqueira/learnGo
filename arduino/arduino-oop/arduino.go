2393d0a33e4e432719c3f3f647bf812271076ad0
Felipe Siqueira Pinheiro <fsiqp@br.ibm.com>
2017-10-02 17:16:44 -0300

    Refactor Arduino package

--- arduino/arduino-oop/arduino.go
+++ arduino/arduino-oop/arduino.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"errors"
+	"log"
 	"reflect"
 	"strconv"
+
+	serial "go.bug.st/serial.v1"
 
 	"gobot.io/x/gobot/drivers/gpio"
 
@@ -24,9 +27,9 @@ func InitDevices() Devices {
 	}
 }
 
-// Len returns the number of devices attached to an Arduino
-func (d *Devices) Len() int {
-	var devices int
+// CountTotal returns the number of devices attached to an Arduino
+func (d *Devices) CountTotal() int {
+	var countTotal int
 	value := reflect.ValueOf(*d)
 
 	for i := 0; i < value.NumField(); i++ {
@@ -34,23 +37,48 @@ func (d *Devices) Len() int {
 
 		switch reflect.TypeOf(field).Kind() {
 		case reflect.Map:
-			devices += reflect.ValueOf(field).Len()
+			countTotal += reflect.ValueOf(field).Len()
 		}
 	}
 
-	return devices
+	return countTotal
+}
+
+// CountPerDevice returns a map[string]int containing the quantity of devices, classified by it's type
+func (d *Devices) CountPerDevice() map[string]int {
+	countPerDevice := make(map[string]int)
+
+	value := reflect.ValueOf(*d)
+
+	for i := 0; i < value.NumField(); i++ {
+		field := value.Field(i).Interface()
+		fieldType := reflect.TypeOf(field).Kind()
+
+		switch fieldType {
+		case reflect.Map:
+			fieldName := value.Type().Field(i).Name
+			fieldLength := reflect.ValueOf(field).Len()
+
+			if fieldLength > 0 {
+				countPerDevice[fieldName] = fieldLength
+			}
+		}
+	}
+
+	return countPerDevice
 }
 
 // Arduino represents a physical Arduino
 type Arduino struct {
 	Name       string
 	Port       string
+	Status     bool
 	Connection *firmata.Adaptor
 	Devices    Devices
 }
 
 // Arduinos represents a slice of pointer of Arduino
-type Arduinos []*Arduino
+type Arduinos map[string]*Arduino
 
 // NewArduino initializes a new Arduino object, based on the serial connection
 func NewArduino(number int, port string) *Arduino {
@@ -60,6 +88,27 @@ func NewArduino(number int, port string) *Arduino {
 		Connection: firmata.NewAdaptor(port),
 		Devices:    InitDevices(),
 	}
+}
+
+// FindArduinos scans all serial ports and initializes all found Arduinos
+func FindArduinos() Arduinos {
+	var (
+		arduinos     = make(Arduinos)
+		serials, err = serial.GetPortsList()
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for number, serial := range serials {
+		// if strings.HasPrefix(serial, "ttyUSB") {
+		arduino := NewArduino(number, serial)
+		arduinos[arduino.Name] = arduino
+		// }
+	}
+
+	return arduinos
 }
 
 // AddDevice appends a new gobot.Device into Arduino.Devices
--- arduino/arduino-oop/main.go
+++ arduino/arduino-oop/main.go
@@ -3,31 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
-
-	serial "go.bug.st/serial.v1"
 )
 
 func main() {
-	var arduinos Arduinos
+	arduinos := FindArduinos()
 
-	serials, err := serial.GetPortsList()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for number, serial := range serials {
-		// if strings.HasPrefix(serial, "ttyUSB") {
-		arduinos = append(arduinos, NewArduino(number, serial))
-		// }
-	}
-
-	for _, arduino := range arduinos {
+	for name, arduino := range arduinos {
 		err := arduino.AddDevice("led", "13")
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(err.Error())
 		}
 
-		fmt.Println("Default Name:", arduino.Name, "Port:", arduino.Port, "Connection:", arduino.Connection.Name(), "Number of Devices:", arduino.Devices.Len())
+		fmt.Printf(
+			"Name: %s\nPort: %s\nDevices: %d\nStatus: %t\nLEDs: %d\n",
+			name,
+			arduino.Port,
+			arduino.Devices.CountTotal(),
+			arduino.Status,
+			arduino.Devices.CountPerDevice()["LED"],
+		)
 	}
-
 }
